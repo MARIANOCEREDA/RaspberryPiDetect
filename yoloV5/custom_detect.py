de3c7c656095af56ce4102c11ebf2a04b741c290
facundocardenas <facu26giol@gmail.com>
2023-08-23 20:19:26 -0300

    calculo diametro stics

--- yoloV5/custom_detect.py
+++ yoloV5/custom_detect.py
@@ -138,7 +138,6 @@ def run_inference(
             # Save results (image with detections)
             if save_img:
                 if dataset.mode == 'image':
-                    cv2.imwrite(str(save_dir / 'image_main.jpeg'), im0s)
                     cv2.imwrite(save_path, im0)
                 else:  # 'video' or 'stream'
                     if vid_path[i] != save_path:  # new video
@@ -195,15 +194,13 @@ def get_main_package(packages:np.array) -> np.array:
         main_package = packages[max_area_index]
     
     else:
-        main_package = packages
-
+        main_package = packages[0]
     return main_package
 
             
 def filter_sticks_within_package(sticks:np.array, package:np.array) -> np.array:
 
     _, x, y, w, h = package
-
     x_max = x + w / 2
     x_min = x - w / 2
     y_max = y + h / 2
@@ -220,10 +217,10 @@ def filter_sticks_within_package(sticks:np.array, package:np.array) -> np.array:
 
     return np.array(filtered_sticks)
 
-def draw_package_and_sticks(image_path, main_package, sticks_within_package, img_size):
+def draw_package_and_sticks(image_path, main_package, sticks_within_package,config_data):
     # Cargar la imagen
     image1 = cv2.imread(image_path)
-    
+    img_size,_,_=image1.shape
     # Dibujar el recuadro del paquete principal en verde
     _, x, y, w, h = main_package*img_size
     #cv2.rectangle(image1, (int(x - w / 2), int(y + h / 2)), (int(x + w / 2), int(y - h / 2)), (0, 255, 0), 2)
@@ -234,8 +231,44 @@ def draw_package_and_sticks(image_path, main_package, sticks_within_package, img
         cv2.circle(image1, (int(x_s), int(y_s)), int((w_s+h_s)/4), (0, 0, 255), 2)
     
     # Guardar la imagen con los recuadros
-    output_path = os.path.join(os.path.dirname(image_path), "image_main_with_boxes.jpeg")
+    output_path = os.path.join(os.path.dirname(__file__),config_data["images"]["results"],"image_main_with_boxes.jpeg")
     cv2.imwrite(output_path, image1)
+
+def cut_image(image_path,config_data):
+    image_cut = cv2.imread(image_path)
+    output_path = os.path.join(os.path.dirname(__file__),config_data["images"]["results"])
+    # Obtener las dimensiones de la imagen y centro
+    hc, wc, _ = image_cut.shape 
+    x_c = wc//2
+    y_c = hc//2
+    # Cortar la imagen en cuatro partes
+    top_left = image_cut[0:y_c, 0:x_c]
+    top_right = image_cut[0:y_c, x_c:wc]
+    bottom_left = image_cut[y_c:hc, 0:x_c]
+    bottom_right = image_cut[y_c:hc, x_c:wc]
+    #Agrandamos las imagenes a 640
+    top_left = cv2.resize(top_left, (wc, hc))
+    top_right = cv2.resize(top_right, (wc, hc))
+    bottom_left = cv2.resize(bottom_left, (wc, hc))
+    bottom_right = cv2.resize(bottom_right, (wc, hc))
+    # Guardar las imágenes cortadas
+    cv2.imwrite(os.path.join(os.path.dirname(__file__),config_data["images"]["results"],'top_left.jpeg'), top_left)
+    cv2.imwrite(os.path.join(os.path.dirname(__file__),config_data["images"]["results"],'top_right.jpeg'), top_right)
+    cv2.imwrite(os.path.join(os.path.dirname(__file__),config_data["images"]["results"],'bottom_left.jpeg'), bottom_left)
+    cv2.imwrite(os.path.join(os.path.dirname(__file__),config_data["images"]["results"],'bottom_right.jpeg'), bottom_right)
+
+def calculate_diameter(img_size, main_package, sticks_within_package,config_data):
+    diam_fardo=130 #diametro del fardo en cm
+    _, x_d, y_d, w_d, h_d = main_package*img_size
+    diam_mainpackage_pixel= (w_d+h_d)/2 #tomamos el diametro en pixeles como el promedio entra la altura y ancho
+    rel_pixeles_cm=diam_mainpackage_pixel/diam_fardo
+    diam_sticks=[]
+    for stick in sticks_within_package:
+        _, x_s, y_s, w_s, h_s=stick*img_size
+        diam_stick_pixel=(w_s+h_s)/2
+        diam_stick_cm=diam_stick_pixel/rel_pixeles_cm
+        diam_sticks.append(diam_stick_cm)
+    return(diam_sticks)
 
 def main():
 
@@ -255,7 +288,6 @@ def main():
     print(config_data)
 
     for key, value in config_data.items():
-
         if key == "sticks" or key == "packages":
 
             run_inference(weights=config_data[key]["weights"],  # model path or triton URL
@@ -288,14 +320,17 @@ def main():
 
     sticks_results_path = os.path.join(os.path.dirname(__file__), config_data["sticks"]["results"], "labels", "image.txt")
     packages_results_path = os.path.join(os.path.dirname(__file__), config_data["packages"]["results"], "labels", "image.txt")
-    image_path = os.path.join(os.path.dirname(__file__), config_data["packages"]["results"], "image_main.jpeg")
+    image_path = os.path.join(os.path.dirname(__file__), config_data["source"], "image.jpeg")
     sticks = read_results_from_txt(sticks_results_path)
     packages = read_results_from_txt(packages_results_path)
     main_package = get_main_package(packages)
     sticks_within_package = filter_sticks_within_package(sticks, main_package)
-    draw_package_and_sticks(image_path, main_package, sticks_within_package,config_data["img_size"])
-
-
+    draw_package_and_sticks(image_path, main_package, sticks_within_package,config_data)
+    #cut_image(image_path,config_data)
+    diameters_sticks=calculate_diameter(config_data["img_size"], main_package, sticks_within_package,config_data)
+    print(diameters_sticks)
+    prom_diameters=sum(diameters_sticks)/len(diameters_sticks)
+    print(prom_diameters)
 if __name__ == '__main__':
     main()
 
