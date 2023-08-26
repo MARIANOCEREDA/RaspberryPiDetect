7b4e7b21b01f2214cbe85ff6bade24defb343425
MARIANOCEREDA <gmarianocereda@gmail.com>
2023-08-26 12:47:35 -0300

    add imports within init

--- yoloV5/detect/main.py
+++ yoloV5/detect/main.py
@@ -1,11 +1,6 @@
 import sys
+from yoloV5.custom_detect import main
 
 if __name__ == "__main__":
-
-    sys.path.append(f"C:/Users/merem/Documents/TESIS/code/deploy/RaspberryPiDetect/yoloV5/utils")
-    sys.path.append(f"C:/Users/merem/Documents/TESIS/code/deploy/RaspberryPiDetect/yoloV5")
-    sys.path.append(f"C:/Users/merem/Documents/TESIS/code/deploy/RaspberryPiDetect/yoloV5/models")
-
-    from yoloV5.custom_detect import main
 
     main()
