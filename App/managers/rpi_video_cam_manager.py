6e2051829b476e5e732d81a3d5e87db92d82881b
MARIANOCEREDA <gmarianocereda@gmail.com>
2023-09-11 18:25:30 -0300

    add rpi video

--- App/managers/rpi_video_cam_manager.py
+++ App/managers/rpi_video_cam_manager.py
@@ -1,1 +1,29 @@
+import time
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from picamera2 import Picamera2, Preview
 
+class RPiCameraManager:
+
+    def __init__(self) -> None:
+        self.picam = Picamera2()
+        config = self.picam.create_preview_configuration()
+        self.picam.configure(config)
+
+    def start_loop(self):
+
+        while True:
+                
+            self.picam.start_preview(Preview.QTGL)
+
+            self.picam.start()
+            self.picam.start_preview()
+            
+            QApplication.processEvents()
+    
+    def capture_photo(self, result_path:str):
+
+        self.picam.capture_file(result_path)
+
+    def finish(self):
+
+        self.picam.close()
