77cfd58d3e7ae1288b5a02523c7d277883e8f62a
MARIANOCEREDA <gmarianocereda@gmail.com>
2023-09-20 20:11:01 -0300

    modify run.sh file

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 # Define variables and paths
 DETECT_FOLDER="/home/mariano/workspace/tesis/RaspberryPiDetect"
-APP_FOLDER="App"
+APP_FOLDER="/home/mariano/workspace/tesis/RaspberryPiDetect/App"
 LOG_FILE="/home/mariano/workspace/tesis/log/run.log"
+
 
 # Initialize log file with a timestamp
 echo "Script started at $(date)" >> "$LOG_FILE"
@@ -12,6 +13,8 @@ echo "Script started at $(date)" >> "$LOG_FILE"
 if [ ! -d "$DETECT_FOLDER" ]; then
     echo "Error: Detect folder not found." >> "$LOG_FILE"
     exit 1
+else
+    echo "Detect folder found."
 fi
 
 # Activate virtual environment
@@ -21,6 +24,8 @@ source "$DETECT_FOLDER/bin/activate" >> "$LOG_FILE" 2>&1
 if [ $? -ne 0 ]; then
     echo "Error: Virtual environment activation failed." >> "$LOG_FILE"
     exit 1
+else
+    echo "Virtual env activated."
 fi
 
 # Go to App folder
@@ -30,8 +35,11 @@ cd "$APP_FOLDER" >> "$LOG_FILE" 2>&1
 if [ $? -ne 0 ]; then
     echo "Error: App folder not found." >> "$LOG_FILE"
     exit 1
+else
+    echo "App folder found."
 fi
 
+echo "Executing app..."
 # Run the app and capture output
 python app_2.py >> "$LOG_FILE" 2>&1
 
@@ -39,6 +47,8 @@ python app_2.py >> "$LOG_FILE" 2>&1
 if [ $? -ne 0 ]; then
     echo "Error: App execution failed." >> "$LOG_FILE"
     exit 1
+else
+    echo "App execution OK."
 fi
 
 # Script completed successfully
