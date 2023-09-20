b65329c4bde21af75f6470ec99f91520106b8df2
MARIANOCEREDA <gmarianocereda@gmail.com>
2023-09-20 19:44:41 -0300

    run everything from bash script

---
+++ run.sh
@@ -0,0 +1,45 @@
+# !/bin/bash
+
+# Define variables and paths
+DETECT_FOLDER="/home/mariano/workspace/tesis/RaspberryPiDetect"
+APP_FOLDER="App"
+LOG_FILE="/home/mariano/workspace/tesis/log/run.log"
+
+# Initialize log file with a timestamp
+echo "Script started at $(date)" >> "$LOG_FILE"
+
+# Check if the detect folder exists
+if [ ! -d "$DETECT_FOLDER" ]; then
+    echo "Error: Detect folder not found." >> "$LOG_FILE"
+    exit 1
+fi
+
+# Activate virtual environment
+source "$DETECT_FOLDER/bin/activate" >> "$LOG_FILE" 2>&1
+
+# Check if activation was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Virtual environment activation failed." >> "$LOG_FILE"
+    exit 1
+fi
+
+# Go to App folder
+cd "$APP_FOLDER" >> "$LOG_FILE" 2>&1
+
+# Check if the App folder exists
+if [ $? -ne 0 ]; then
+    echo "Error: App folder not found." >> "$LOG_FILE"
+    exit 1
+fi
+
+# Run the app and capture output
+python app_2.py >> "$LOG_FILE" 2>&1
+
+# Check if the app ran successfully
+if [ $? -ne 0 ]; then
+    echo "Error: App execution failed." >> "$LOG_FILE"
+    exit 1
+fi
+
+# Script completed successfully
+echo "Script completed at $(date)" >> "$LOG_FILE"
